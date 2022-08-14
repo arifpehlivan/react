8e017f096102d0a674b675024b8dea8e684ef29b
arifpehlivan <arifpehlivanapap@gmail.com>
2022-08-14 16:54:16 +0300

    add form

--- react-validation/src/App.js
+++ react-validation/src/App.js
@@ -1,9 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import FormInput from './components/FormInput';
 
-function App() {
+const App = () => {
+  const [username,setUsername] = useState("")
+  console.log(username);
   return (
-    <div>
-      Hello World
+    <div className="app">
+      <form>
+        <FormInput placeholder="Username" setUsername={setUsername}/>
+        <FormInput placeholder="Full Name"/>
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="Password"/>
+      </form>
     </div>
   );
 }
